e2a45dc6f9b105133ca3322063a891deadc1c6a4
AlexeyParshin <dip1771@gmail.com>
2016-05-19 01:18:51 +0300

    try to commit

--- src/Example.java
+++ src/Example.java
@@ -242,6 +242,7 @@ class SwingDemo implements ActionListener {
 public class Example {
     public static void main(String[] args) {
         System.out.print("ok");
-        System.out.print(", will that bitch commit or no?");
+        System.out.println(", will that bitch commit or no?");
+        System.out.print("ok,everyfin is fine");
     }
 }
