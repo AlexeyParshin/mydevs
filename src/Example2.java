d7a155e3a365f424c588601b7b71203a009394ee
AlexeyParshin <dip1771@gmail.com>
2016-09-26 22:13:48 +0300

    -reset -get number of chars method -numCh field in each class

---
+++ src/Example2.java
@@ -0,0 +1,7 @@
+import javaBookExamples.exceptionPack.excDemo;
+
+public class Example2 {
+    public static void main(String[] args) {
+        excDemo demo = new excDemo();
+    }
+}
---
+++ src/javaBookExamples/exceptionPack/excDemo.java
@@ -0,0 +1,75 @@
+package javaBookExamples.exceptionPack;
+
+/**
+ * Created by dip17_000 on 25.09.2016.
+ */
+
+class arrays {
+    private String[] nStrArray;
+    private String[] dStrArray;
+    private int nlgt;
+    private int dlgt;
+
+    arrays() {
+        nStrArray = "1,2,32,54,1,54,43".split(",");
+        dStrArray = "0,34,0,12,54,25".split(",");
+        nlgt = nStrArray.length;
+        dlgt = dStrArray.length;
+    }
+
+    void showNumer() {
+        for (String str : nStrArray) {
+            System.out.print(str+" ");
+        }
+        System.out.println();
+    }
+
+    void showDenom() {
+        for (String str : dStrArray) {
+            System.out.print(str+" ");
+        }
+        System.out.println();
+    }
+
+    int get_nNumber(int _ind) {
+        int intNum = Integer.parseInt(nStrArray[_ind]);
+        return intNum;
+    }
+
+    int get_dNumber(int _ind) {
+        int intDen = Integer.parseInt(dStrArray[_ind]);
+        return intDen;
+    }
+
+    int getnumerLenght() {
+        return nlgt;
+    }
+
+    int getdenomLenght() {
+        return dlgt;
+    }
+}
+
+
+public class excDemo {
+    public excDemo() {
+        arrays obj = new arrays();
+        String res = new String();
+
+        for (int i = 0; (i < obj.getnumerLenght()) && (i < obj.getdenomLenght()); i++) {
+            try {
+                res+=String.format("%.2f",((double) obj.get_nNumber(i) / (double) obj.get_dNumber(i)));
+                if ((i != obj.getnumerLenght() - 1) && (i != obj.getdenomLenght() - 1)) {
+                    res += ", ";
+                }
+            } catch (ArithmeticException exc) {
+                res += "inf, ";
+            }
+        }
+        obj.showNumer();
+        obj.showDenom();
+        System.out.println("\nAfter dividing: ");
+        System.out.println(res);
+    }
+
+}
